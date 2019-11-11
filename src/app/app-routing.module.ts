a623675addb168ea4783663424525f4989b89334
Will Walker <wwalker@udel.edu>
2019-11-11 13:53:42 -0500

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { InfoComponent } from "./info/info.component";
 import { HomeComponent } from "./home/home.component";
+import { ListsComponent } from "./lists/lists.component";
+import { HadComponent } from "./lists/had/had.component";
+import { HaveComponent } from "./lists/have/have.component";
+import { WantComponent } from "./lists/want/want.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
-  { path: "info", component: InfoComponent }
+  { path: "info", component: InfoComponent },
+  { path: "lists", component: ListsComponent },
+  { path: "lists/had", component: HadComponent },
+  { path: "lists/have", component: HaveComponent },
+  { path: "lists/want", component: WantComponent }
 ];
 
 @NgModule({
