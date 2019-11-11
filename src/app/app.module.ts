c3eeadce33aae73e140c1bc189582b8fb53ee69d
Will Walker <wwalker@udel.edu>
2019-11-10 19:58:24 -0500

    add app attributes to the nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,24 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatIconModule } from "@angular/material/icon";
-import { NavigationComponent } from './navigation/navigation.component';
-import { UserCardComponent } from './user-card/user-card.component';
+import { MatCardModule } from "@angular/material/card";
+
+import { NavigationComponent } from "./navigation/navigation.component";
+import { UserCardComponent } from "./user-card/user-card.component";
+import { MatButtonModule } from "@angular/material/button";
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, UserCardComponent],
+  declarations: [AppComponent, NavigationComponent, UserCardComponent, LayoutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
