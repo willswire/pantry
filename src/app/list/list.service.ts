43938d254daf6ba6916c4d8e97a780305f43ad84
Connor Onweller <onweller@udel.edu>
2019-11-19 23:41:30 -0500

    Added service to create, get, and delete list

---
+++ src/app/list/list.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ListService {
+  private headers = new HttpHeaders({
+    "Content-Type": "application/json"
+  });
+
+  constructor(private _http: HttpClient) {}
+
+  public createList(title: String) {
+    return this._http.post(
+      `http://localhost:3000/api/lists/`,
+      { title: title },
+      { headers: this.headers },
+    )
+    .pipe(
+      map((list: any) => {
+        return list;
+      })
+    );
+  }
+
+  public deleteList(listID: String) {
+    return this._http.delete(
+      `http://localhost:3000/api/lists/${listID}`,
+      { headers: this.headers, observe: 'response', responseType: 'text'  },
+    )
+    .pipe(
+      map((res: any) => {
+        return res;
+      })
+    );
+  }
+
+  public getListByID(listID: String) {
+    return this._http.get<any>(
+      `http://localhost:3000/api/lists/${listID}`,
+      { headers: this.headers }
+    )
+    .pipe(
+      map((list: any) => {
+        return list;
+      })
+    );
+  }
+}
