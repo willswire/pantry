0c32ad92a1e2cb95e259db3b9fb91e6d3bae2671
Will Walker <wwalker@udel.edu>
2019-11-20 08:21:24 -0500

    remove home component/page/route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { InfoComponent } from "./info/info.component";
-import { HomeComponent } from "./home/home.component";
 import { ListPageComponent } from "./list-page/list-page.component";
 
 const routes: Routes = [
-  { path: "", component: HomeComponent, pathMatch: "full" },
+  { path: "", component: ListPageComponent, pathMatch: "full" },
   { path: "info", component: InfoComponent },
   { path: "lists", component: ListPageComponent }
 ];
