3e828ea2de7dcb7cbb072559ccf846b0ceccb1b9
Connor Onweller <onweller@udel.edu>
2019-11-23 20:41:08 -0500

    Added Error Interceptor

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -5,7 +5,17 @@ import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { FormsModule } from '@angular/forms'; // <--- JavaScript import from Angular
-import { MatCardModule, MatFormFieldModule, MatProgressSpinnerModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatSelectModule } from '@angular/material';
+import {
+  MatCardModule,
+  MatFormFieldModule,
+  MatProgressSpinnerModule,
+  MatInputModule,
+  MatButtonModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatSelectModule
+} from '@angular/material';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 
 @NgModule({
@@ -25,6 +35,7 @@ import { MatCardModule, MatFormFieldModule, MatProgressSpinnerModule, MatInputMo
   ],
   providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
   exports: [LoginComponent, RegisterComponent]
 })
