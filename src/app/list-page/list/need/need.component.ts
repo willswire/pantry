94d19eb60522d5ac88cd41eb2a1b013d80c1e273
Will Walker <wwalker@udel.edu>
2019-11-25 14:45:50 -0500

    pull items from the database

--- src/app/list-page/list/need/need.component.ts
+++ src/app/list-page/list/need/need.component.ts
@@ -9,38 +9,14 @@ import { DialogData } from "../list.component";
 })
 export class NeedComponent implements OnInit, DialogData {
   listName: string;
-  listRef: string;
-
-  items: Object[] = [
-    {
-      name: "Cake",
-      expires: new Date(Date.now()),
-      icon: "cake"
-    },
-    {
-      name: "Coffee",
-      expires: new Date(Date.now()),
-      icon: "free_breakfast"
-    },
-    {
-      name: "Steak",
-      expires: new Date(Date.now()),
-      icon: "outdoor_grill"
-    },
-    {
-      name: "Spinach",
-      expires: new Date(Date.now()),
-      icon: "eco"
-    }
-  ];
+  items: Object[];
 
   constructor(
     public dialogRef: MatDialogRef<NeedComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) {
-    this.items;
+    if (data.items) this.items = data.items;
     this.listName = data.listName;
-    this.listRef = data.listRef;
   }
 
   onNoClick(): void {
