51631af6288f3bc3a79ded8ba7f55d4fd624f2a2
Scott Benton <scott.ma.benton@gmail.com>
2019-12-02 16:59:57 -0500

    Started refactor of landing page. Still need to make some changes surrounding the width of everything on larger screens.

---
+++ src/assets/DEVELOPERS.ts
@@ -0,0 +1,27 @@
+export const DEVELOPERS = [
+  {
+    name: "Muhammet Aydin",
+    githubUsername: "MadeByAydin",
+    email: "muhammet@udel.edu"
+  },
+  {
+    name: "Scott Benton",
+    githubUsername: "scottbenton",
+    email: "scott.ma.benton@gmail.com"
+  },
+  {
+    name: "CJ Lambert",
+    githubUsername: "charleslambert98",
+    email: "clambert@udel.edu"
+  },
+  {
+    name: "Connor Onweller",
+    githubUsername: "conweller",
+    email: "onweller@udel.edu"
+  },
+  {
+    name: "Will Walker",
+    githubUsername: "willswire",
+    email: "wwalker@udel.edu"
+  }
+];
