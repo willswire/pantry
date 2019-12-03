21cec7726aadf947f419f171b42f7142e6853999
Will Walker <wwalker@udel.edu>
2019-12-02 20:40:48 -0500

    delete items from lists

--- src/app/list-page/list/need/need.component.ts
+++ src/app/list-page/list/need/need.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 import { DialogData } from "../list.component";
+import { ListService } from "src/app/list/list.service";
 
 @Component({
   selector: "app-need",
@@ -9,14 +10,17 @@ import { DialogData } from "../list.component";
 })
 export class NeedComponent implements OnInit, DialogData {
   listName: string;
-  items: Object[];
+  items: any;
+  listRef: string;
 
   constructor(
     public dialogRef: MatDialogRef<NeedComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    public api: ListService
   ) {
     if (data.items) this.items = data.items;
     this.listName = data.listName;
+    this.listRef = data.listRef;
   }
 
   onNoClick(): void {
@@ -24,4 +28,15 @@ export class NeedComponent implements OnInit, DialogData {
   }
 
   ngOnInit() {}
+
+  removeItem(itemID: string) {
+    console.log("The list is: " + this.listRef);
+    console.log("The item is: " + itemID);
+    this.api.deleteItem(this.listRef, itemID).subscribe(result => {
+      result;
+    });
+    this.items = this.items.filter(item => {
+      return item._id !== itemID;
+    });
+  }
 }
